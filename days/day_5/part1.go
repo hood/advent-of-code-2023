991d87958ef94cb84f6d45ff2ebf5025419d5d08
hood <hood@null.net>
2023-12-06 22:21:58 +0100

    x

--- days/day_5/part1.go
+++ days/day_5/part1.go
@@ -7,7 +7,7 @@ import (
 )
 
 func day5Part1() {
-	println("\n\n***** Day 4.1 ****")
+	println("\n\n***** Day 5.1 ****")
 
 	fileContent, error := os.ReadFile("./days/day_5/part_1_test_input.txt")
 	if error != nil {
