0c5fc01bb4cc588e24c916ca589bae177323a85f
hood <hood@null.net>
2023-12-07 01:25:01 +0100

    x

--- days/day_5/part2.go
+++ days/day_5/part2.go
@@ -9,7 +9,7 @@ import (
 func day5Part2() {
 	println("\n\n***** Day 5.2 ****")
 
-	fileContent, error := os.ReadFile("./days/day_5/part_1_test_input.txt")
+	fileContent, error := os.ReadFile("./days/day_5/input.txt")
 	if error != nil {
 		panic(error)
 	}
@@ -55,14 +55,3 @@ func day5Part2() {
 
 	println("Result", "->", lowestSeedLocation)
 }
-
-// your answer is too high: 289863851
-
-// In the above example, the lowest location number can be obtained from seed
-// number 82, which corresponds to soil 84, fertilizer 84, water 84, light 77,
-// temperature 45, humidity 46, and location 46. So, the lowest location number
-// is 46.
-//
-// Why 82 and not 79?
-// Gut says: Because the seeds range is 79-92, and 82 is the first
-// seed in that range that produces a location number of 46.
