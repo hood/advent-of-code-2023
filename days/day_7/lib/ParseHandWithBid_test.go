d71e2c57bf31e5f4acb3b644a38c5ec2409e74de
hood <hood@null.net>
2023-12-07 23:53:18 +0100

    x

--- days/day_7/lib/ParseHandWithBid_test.go
+++ days/day_7/lib/ParseHandWithBid_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestParseHandWithBid(t *testing.T) {
-	str := "23456 1"
+	str := "23456 1234"
 
 	handWithBid := ParseHandWithBid(str)
 
 	shared.AssertEqual(t, "23456", "23456")
-	shared.AssertEqual(t, 1, handWithBid.Bid)
+	shared.AssertEqual(t, 1234, handWithBid.Bid)
 }
