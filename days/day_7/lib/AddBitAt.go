3d4fa6dafc555bf92691539de7ed6e3a5980ba7f
hood <hood@null.net>
2023-12-07 15:21:31 +0100

    x

--- days/day_7/lib/AddBitAt.go
+++ days/day_7/lib/AddBitAt.go
@@ -1,5 +1,29 @@
 package lib
 
-func AddBitAt(data uint64, position uint8) uint64 {
-	return data + 1<<(position*4)
+// func AddBitAt(data uint64, position uint8) uint64 {
+// 	// return data + 1<<(position*4)
+
+// 	// literally add a bit to the left starting
+// 	// from the given position.
+// 	// example
+// 	// 0000 0001
+// 	// after adding a bit at position 2
+// 	// becomes
+// 		 0000 0011
+// }
+
+func AddBitAt(data uint64, value uint8) uint64 {
+	position := (value) * 4
+
+	for i := uint8(0); i < 4; i++ {
+		bitPos := position + i
+		bitmask := uint64(1) << bitPos
+
+		if (data & bitmask) == 0 {
+			data |= bitmask
+			break
+		}
+	}
+
+	return data
 }
