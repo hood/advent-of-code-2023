cc161956a7d1b77c8a96d3ca0c27aaf0647aef40
hood <hood@null.net>
2023-12-08 22:29:26 +0100

    x

--- days/day_8/lib/FindInMapWithCompass.go
+++ days/day_8/lib/FindInMapWithCompass.go
@@ -1,36 +1,5 @@
 package lib
 
-func FindInMapWithCompass(m *Map, c *Compass, target string) (bool, int) {
-	level := 0
-	current := m.Head
-	direction := c.NextDirection()
-
-	for current != nil {
-		if current.ID == target {
-			return true, level
-		}
-
-		// println(
-		// 	"Current: ", current.ID,
-		// 	"Level: ", level,
-		// 	"Next: ", string(direction),
-		// 	"Left: ", current.Left,
-		// 	"Right: ", current.Right,
-		// )
-
-		if direction == 'L' {
-			current = current.Left
-		} else if direction == 'R' {
-			current = current.Right
-		}
-
-		direction = c.NextDirection()
-		level++
-	}
-
-	return false, -1
-}
-
 func FindInHashMapWithCompass(hashMap HashMap, c *Compass, target string) (bool, int) {
 	level := 0
 	current := hashMap["AAA"]
--- days/day_8/lib/FindInMapWithCompass_test.go
+++ days/day_8/lib/FindInMapWithCompass_test.go
@@ -5,28 +5,6 @@ import (
 	"testing"
 )
 
-func TestFindInMapWithCompass(t *testing.T) {
-	lines := []string{
-		"LLR",
-		"",
-		"AAA = (BBB, BBB)",
-		"BBB = (AAA, ZZZ)",
-		"ZZZ = (ZZZ, ZZZ)",
-	}
-
-	c := NewCompass()
-
-	c.FromLine(lines[0])
-	lines = lines[2:]
-
-	myMap := MapFromLines(lines)
-
-	found, level := FindInMapWithCompass(myMap, c, "ZZZ")
-
-	shared.AssertEqual(t, true, found)
-	shared.AssertEqual(t, 6, level)
-}
-
 func TestFindInHashMapWithCompass(t *testing.T) {
 	lines := []string{
 		"LLR",
