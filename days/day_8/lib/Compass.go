79c11a7764a797998c43ae4bec6bbcd238abb8c0
hood <hood@null.net>
2023-12-08 15:43:29 +0100

    x

--- days/day_8/lib/Compass.go
+++ days/day_8/lib/Compass.go
@@ -8,17 +8,21 @@ type Compass struct {
 func NewCompass() *Compass {
 	return &Compass{
 		Order: make([]rune, 0),
+		step:  0,
 	}
 }
 
 func (c *Compass) NextDirection() rune {
+	direction := c.Order[c.step]
+
 	if c.step == len(c.Order)-1 {
+		// TODO: Without a reset this can become a steps counter.
 		c.step = 0
 	} else {
 		c.step++
 	}
 
-	return c.Order[c.step]
+	return direction
 }
 
 func (c *Compass) FromLine(line string) *Compass {
--- days/day_8/lib/Compass_test.go
+++ days/day_8/lib/Compass_test.go
@@ -10,11 +10,13 @@ func TestCompass(t *testing.T) {
 	c := NewCompass()
 	c.FromLine("LLR")
 
+	shared.AssertEqual(t, "LLR", string(c.Order))
+
 	expected := []rune{'L', 'L', 'R', 'L', 'L', 'R'}
 
 	for step, expectedRune := range expected {
-		next := c.NextDirection()
+		direction := c.NextDirection()
 
-		shared.AssertEqual(t, string(expectedRune), string(next), fmt.Sprintf("Step %v", step))
+		shared.AssertEqual(t, string(expectedRune), string(direction), fmt.Sprintf("Step %v", step))
 	}
 }
