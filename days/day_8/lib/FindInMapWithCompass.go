4f6710a29e8aac206e77b7eeb7541aa68e3528d3
hood <hood@null.net>
2023-12-08 15:50:11 +0100

    x

--- days/day_8/lib/FindInMapWithCompass.go
+++ days/day_8/lib/FindInMapWithCompass.go
@@ -2,22 +2,29 @@ package lib
 
 func FindInMapWithCompass(m *Map, c *Compass, target string) (bool, int) {
 	level := 0
-
 	current := m.Head
+	direction := c.NextDirection()
 
 	for current != nil {
 		if current.ID == target {
 			return true, level
 		}
 
-		next := c.NextDirection()
+		println(
+			"Current: ", current.ID,
+			"Level: ", level,
+			"Next: ", string(direction),
+			"Left: ", current.Left.ID,
+			"Right: ", current.Right.ID,
+		)
 
-		if next == 'L' {
+		if direction == 'L' {
 			current = current.Left
-		} else if next == 'R' {
+		} else if direction == 'R' {
 			current = current.Right
 		}
 
+		direction = c.NextDirection()
 		level++
 	}
 
