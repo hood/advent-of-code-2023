2f11bda322c1a4c8e306d3b4dc79934dec867e41
hood <hood@null.net>
2023-12-09 17:22:33 +0100

    x

---
+++ days/day_9/part1_test.go
@@ -0,0 +1,20 @@
+package day_9
+
+import (
+	"adventofcode2023/days/day_9/lib"
+	"adventofcode2023/days/shared"
+	"testing"
+)
+
+func TestDay9Part1(t *testing.T) {
+	lines := shared.ReadFile("./test_input.txt")
+
+	result := 0
+
+	for _, line := range lines {
+		s := lib.SequenceFromString(line)
+		result += s.FinalValue()
+	}
+
+	shared.AssertEqual(t, 114, result)
+}
